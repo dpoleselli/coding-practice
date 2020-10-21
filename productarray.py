2110490f12d66ef41341fcd88c87f0d4a6df9a1d
d_poleselli <dpoleselli@outsideopen.com>
2020-10-20 21:03:11 -0700

    account for zeros in the array

--- productarray.py
+++ productarray.py
@@ -1,13 +1,26 @@
-import sys
 
 def product_array(start):
 	total = 1
 	final = []
+	zeros = 0
 	for value in start:
-		total = total * value
+		if value != 0:
+			total = total * value
+		else:
+			zeros += 1
+			if zeros > 1:
+				break
 
 	for value in start:
-		new_value = total/value if len(start) != 1 else value
+		new_value = value
+		if zeros > 1:
+			new_value = 0
+		elif value == 0:
+			new_value = total
+		elif zeros == 1:
+			new_value = 0
+		elif len(start) != 1:
+			new_value = total/value
 		final.append(new_value)
 
 	return final
--- productarray_test.py
+++ productarray_test.py
@@ -15,6 +15,12 @@ class ProductArrayTest(unittest.TestCase):
     def test_one_element(self):
         self.assertEqual(productarray.product_array([5]), [5])
 
+    def test_with_one_zero(self):
+        self.assertEqual(productarray.product_array([5, 4, 3, 2, 1, 0]), [0, 0, 0, 0, 0, 120])
+
+    def test_with_multiple_zeros(self):
+        self.assertEqual(productarray.product_array([5, 0, 3, 0, 1, 0]), [0, 0, 0, 0, 0, 0])
+
     def test_negative_values(self):
         self.assertEqual(productarray.product_array([-1, 2, 3, 4, -5]), [-120, 60, 40, 30, -24])
 
